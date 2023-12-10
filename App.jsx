422165641ec7d86da06b85c7524786c8175cfec0
hadi3456 <139625221+hadi3456@users.noreply.github.com>
2023-12-10 02:29:18 -0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import ImageSlider from './components/ImageSlider';
+import { SliderData } from './components/SliderData';
+import Todo from './todo/Todo';
+
+function App() {
+<Todo/>
+}
+
+export default App;
